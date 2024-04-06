941e84b1ad76da229f71759f6f60ffabdeda63e5
hiallallall <evgeny.pasichnik@yandex.ru>
2024-04-07 01:58:40 +0700

    Update Main.java

--- testovoe/src/Main.java
+++ testovoe/src/Main.java
@@ -3,88 +3,126 @@ import jdk.jshell.spi.ExecutionControl;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+
 import java.util.SimpleTimeZone;
 
 public class Main {
-    public static void main (String [] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Ввести:");
-        String string =s.nextLine();
-        String[] strArray = string.split(" ");
-        System.out.println(Arrays.toString(strArray));
-        String[] roman = {"","I","II","III","IV","V","VI","VII","VIII","IX","X"};
+    static int x;
+    static int y;
+    static final String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
-        int x;
-        int y;
-        boolean arabic = true;
-        System.out.println(strArray.length);
-        if (strArray.length != 3) {
-            throw new Error("");
-        }
-
+    public static boolean isArabic(String[] strArray) {
         int xRoman = 0;
-         for (int i = 0; i < roman.length;i++) {
-             if (strArray[0].equals(roman[i])){
-                 xRoman = i;
-             }
-         }
+        for (int i = 0; i < roman.length; i++) {
+            if (strArray[0].equals(roman[i])) {
+                xRoman = i;
+            }
+        }
         int yRoman = 0;
-        for (int i = 0; i < roman.length;i++) {
-            if (strArray[2].equals(roman[i])){
+        for (int i = 0; i < roman.length; i++) {
+            if (strArray[2].equals(roman[i])) {
                 yRoman = i;
             }
         }
-        if (xRoman > 0 && yRoman > 0)  {//!!!!!
-             x = xRoman;
-             y = yRoman;
-             arabic = false;
-        }else {
-            x = Integer.parseInt(strArray[0]); // arabic
+        if (xRoman > 0 && yRoman > 0) {
+            x = xRoman;
+            y = yRoman;
+            return false;
+        }
+        return true;
+    }
+
+    public static int calculate(String operand, int x, int y) {
+        if (operand.equals("+")) { //start operandCheck
+            return x + y;
+        } else if (operand.equals("-")) {
+            return x - y;
+        } else if (operand.equals("*")) {
+            return x * y;
+        } else if (operand.equals("/")) {
+            return x / y;
+        } else {
+            throw new Error("wrong operand");
+        }
+        //                switch (strArray[1]){
+//                    case "-" -> answer = x-y;
+//                    case "+" -> answer = x+y;
+//                    case "/" -> answer = x/y;
+//                    case "*" -> answer = x*y;
+//                    default -> throw new Error("wrong operand");
+//                }
+    }
+
+    public static String calculateRoman(int answer) {
+        StringBuilder axe = new StringBuilder();
+        if (answer == 100) {
+            System.out.println("C");
+        } else if (answer > 10) {
+            if (answer >= 50) {
+                answer = answer - 50;
+                axe.append("L");
+            }
+            while (answer >= 10) {
+                answer = answer - 10;
+                axe.append("X");
+            }
+            axe.append(roman[answer]);
+//                        String romanAnswer = roman[answer] + axe;
+//                        char[] toCharArr = romanAnswer.toCharArray();
+//                        String result = "";
+//                        for (int i = toCharArr.length - 1; i >= 0; i--) {
+//                            result = result + toCharArr[i];
+//                        }
+
+        } else {
+            axe.append(roman[answer]);
+        }
+        return axe.toString();
+    }
+
+    ;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ввести:");
+        String string = s.nextLine();
+        String[] strArray = string.split(" ");
+        System.out.println(Arrays.toString(strArray));
+
+
+
+        if (strArray.length != 3) {
+            throw new Error("");
+        }
+        boolean arabic = isArabic(strArray);
+        if (arabic) {
+            x = Integer.parseInt(strArray[0]);
             y = Integer.parseInt(strArray[2]);
         }
         int answer;
-System.out.println("x= " + x);
-System.out.println("y= " + y);
-        String axe = "";
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+
         if (x < 0 || x > 10) {
             throw new Error("x >< 10");
         } // check for number
-        if (y < 0 || y > 10){
-            throw new Error("y >< 10 o");
+        if (y < 0 || y > 10) {
+            throw new Error("y >< 10");
         } // check for number
-                if (strArray[1].equals("+")) { //start operandCheck
-                    answer = x+y;
-                }else if (strArray[1].equals("-")) {
-                    answer = x-y;
-                }else if (strArray[1].equals("*")) {
-                    answer = x*y;
-                }else if (strArray[1].equals("/")) {
-                    answer = x/y;
-                }else {
-                    throw new Error("wrong operand");
-                }
 
-                if (answer < 0 && xRoman == x) {
-                    throw new Error("Roman answer cant be negative");
-                }
+        answer = calculate(strArray[1], x, y);
 
-                if (arabic) {
-                    Integer answerArabic = answer;
-                    System.out.println(answerArabic.toString());
-                }else {
-                    if (answer == 100) {
-                        System.out.println("C");
-                    } else if (answer > 10) {
-                        while (answer > 10) {
-                            answer = answer - 10;
-                            axe = "X" + axe;
-                        }
-                        System.out.println(axe+roman[answer]);
-                    } else {
-                        System.out.println(roman[answer]);
-                    }
-                }
 
+        if (answer < 0 && !arabic) {
+            throw new Error("Roman answer cant be negative");
+        }
+
+        if (arabic) {
+
+            System.out.println(answer);
+        } else {
+            System.out.println(calculateRoman(answer));
+        }
     }
 
 }
